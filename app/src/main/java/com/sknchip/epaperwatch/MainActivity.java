de84b05951b11d7ebdf08554a82f1c536ff3ac5f
Maciej Brzeski <43964891+linuxhuskarl@users.noreply.github.com>
2019-05-18 00:22:45 +0200

    Implemented reading notification count.

--- app/src/main/java/com/sknchip/epaperwatch/MainActivity.java
+++ app/src/main/java/com/sknchip/epaperwatch/MainActivity.java
@@ -1,13 +1,92 @@
 package com.sknchip.epaperwatch;
 
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.provider.Settings;
+import android.service.notification.NotificationListenerService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String ENABLED_NOTIFICATION_LISTENERS = "enabled_notification_listeners";
+    private static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView notificationCountTextView;
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!isNotificationServiceEnabled()){
+            AlertDialog enableNotificationListenerAlertDialog = buildNotificationServiceAlertDialog();
+            enableNotificationListenerAlertDialog.show();
+        }
+
+        notificationCountTextView = findViewById(R.id.nCount);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.sknchip.epaperwatch");
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int notificationCount = intent.getIntExtra("Notification Count", -1);
+                notificationCountTextView.setText(""+notificationCount);
+            }
+        };
+        registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    private AlertDialog buildNotificationServiceAlertDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(R.string.notification_listener_service);
+        alertDialogBuilder.setMessage(R.string.notification_listener_service_explanation);
+        alertDialogBuilder.setPositiveButton(R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+                    }
+                });
+        alertDialogBuilder.setNegativeButton(R.string.no,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // If you choose to not enable the notification listener
+                        // the app. will not work as expected
+                    }
+                });
+        return(alertDialogBuilder.create());
+    }
+
+    private boolean isNotificationServiceEnabled() {
+        String pkgName = getPackageName();
+        final String flat = Settings.Secure.getString(getContentResolver(),
+                ENABLED_NOTIFICATION_LISTENERS);
+        if (!TextUtils.isEmpty(flat)) {
+            final String[] names = flat.split(":");
+            for (int i = 0; i < names.length; i++) {
+                final ComponentName cn = ComponentName.unflattenFromString(names[i]);
+                if (cn != null) {
+                    if (TextUtils.equals(pkgName, cn.getPackageName())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
--- app/src/main/java/com/sknchip/epaperwatch/NotificationListener.java
+++ app/src/main/java/com/sknchip/epaperwatch/NotificationListener.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 
 public class NotificationListener extends NotificationListenerService {
+
+    public NotificationListener(){
+        super();
+        Log.d(TAG,"Service created!");
+    }
+
+    private static final String TAG = NotificationListener.class.getSimpleName();
     @Override
     public IBinder onBind(Intent intent) {
         return super.onBind(intent);
@@ -13,11 +21,28 @@ public class NotificationListener extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn){
-        // TODO update notification count upon posting new one
+        Log.d(TAG, "Posted: " + sbn.getTag());
+        broadcastNotificationCount();
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn){
-        // TODO update notification count upon removing one
+        Log.d(TAG, "Removed!" + sbn.getTag());
+        broadcastNotificationCount();
+    }
+
+    @Override
+    public void onListenerConnected() {
+        super.onListenerConnected();
+        Log.d(TAG,"Listener connected!");
+        broadcastNotificationCount();
+    }
+
+    private void broadcastNotificationCount() {
+        Intent intent = new  Intent("com.sknchip.epaperwatch");
+        int notificationCount = getActiveNotifications().length;
+        intent.putExtra("Notification Count", notificationCount);
+        sendBroadcast(intent);
+        Log.d(TAG, "Count: "+notificationCount);
     }
 }
